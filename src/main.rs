b29b07f7873ed76b840ea4aa924418deb2945e88
rednithin <reddy.nithinpg@live.com>
2019-11-02 13:07:07 +0530

    Made changes to get peer.

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,10 @@
-use actix_web::{web, App, HttpServer, Responder, Result };
+use actix_web::{web, App, HttpServer, Responder, Result, HttpRequest, http::header };
 use rug::Integer;
 
 fn index() -> impl Responder {
     "Hello, World".to_string()
 }
+
 fn factorial_iterative(n: u32) -> Integer {
     let mut result = Integer::from(n);
     for x in 2..n {
@@ -17,14 +18,23 @@ fn factorial_iter_handler(path: web::Path<(u32,)>) -> Result<String> {
     Ok(format!("{}", factorial_iterative(path.0)))
 }
 
+fn ip_address_handler(req: HttpRequest) -> impl Responder {
+    if let Some(peer) = req.peer_addr() {
+        peer.ip().to_string()
+    } else {
+        "Peer info is unavailable.".to_string()
+    }
+}
+
 
 fn main() {
     HttpServer::new(|| {
         App::new()
             .route("/", web::get().to(index))
             .route("/factorial/{number}", web::get().to(factorial_iter_handler))
+            .route("/peer", web::get().to(ip_address_handler))
     })
-    .bind("127.0.0.1:8000")
+    .bind("0.0.0.0:8000")
     .unwrap()
     .run()
     .unwrap();
